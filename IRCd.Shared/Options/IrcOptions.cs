3b9a2b4741927b856a0dba502d23e3e0ae4462a3
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-12-28 21:23:26 +0200

    Add IrcOptions class for IRC server configuration
    Created IrcOptions.cs in IRCd.Shared.Options namespace.
    Added IrcPort and BindAddress properties with default values
    to support IRC server port and bind address configuration.

---
+++ IRCd.Shared/Options/IrcOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace IRCd.Shared.Options
+{
+    public class IrcOptions
+    {
+        public int IrcPort { get; set; } = 6667;
+
+        public string BindAddress { get; set; } = "0.0.0.0";
+    }
+}
