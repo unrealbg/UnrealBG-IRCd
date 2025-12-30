9321e5510f4f13e0d97065446284cf566df30d24
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2025-12-30 21:16:36 +0200

    Add ISessionRegistry interface for session management
    Introduced the ISessionRegistry interface in the IRCd.Core.Abstractions namespace. It provides methods to retrieve a session by connection ID and to get all active client sessions as a read-only collection.

---
+++ IRCd.Core/Abstractions/ISessionRegistry.cs
@@ -0,0 +1,11 @@
+﻿namespace IRCd.Core.Abstractions
+{
+    using System.Collections.Generic;
+
+    public interface ISessionRegistry
+    {
+        bool TryGetSession(string connectionId, out IClientSession? session);
+
+        IReadOnlyCollection<IClientSession> GetAll();
+    }
+}
